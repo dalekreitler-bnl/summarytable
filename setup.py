cbe7180e6a3c7262d1930d6c3d6dc349aa5b21f5
Kreitler, Dale <dkreitler@bnl.gov>
2022-05-16 22:21:00 -0400

    update version for PyPI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='summarytable',
-    version='0.1.2',
+    version='0.1.3',
     author='Dale Kreitler',
     author_email='dkreitler@bnl.gov',
     packages=['summarytable',],
