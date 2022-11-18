8fa55c9fe030ab499309d75caaf727db924128f3
Kreitler, Dale <dkreitler@bnl.gov>
2022-11-17 20:42:24 -0500

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='summarytable',
-    version='0.1.6',
+    version='0.1.8',
     author='Dale Kreitler',
     author_email='dkreitler@bnl.gov',
     packages=['summarytable',],
